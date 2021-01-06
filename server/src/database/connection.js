db636d4564cd2f957dc3cc8b2cceef0125c833f1
juniorHotes <juniorhotes53@gmail.com>
2021-01-06 18:46:14 -0300

    Database connection.

---
+++ server/src/database/connection.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+
+const connection = new Sequelize('react_blog', 'root', 'junior150692', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+module.exports = connection
