5f7f5e632474c8b6c4b1a5c1552692e76fa9c6d1
juniorHotes <juniorhotes53@gmail.com>
2021-01-13 16:10:24 -0300

    Defined timezone.

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize('react_blog', 'root', 'junior150692', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection
