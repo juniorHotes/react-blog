538691c4174b74effa782e10d3b26da5874ec62b
juniorHotes <juniorhotes53@gmail.com>
2021-01-13 16:11:53 -0300

    Field validations entered.

--- server/src/database/migrations/category.js
+++ server/src/database/migrations/category.js
@@ -4,11 +4,20 @@ const connection = require('../connection')
 const Category = connection.define('category', {
     title: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode ser vazio."
+            },
+            len: {
+                args: [4, 40],
+                msg: "Forneça um texto que seja maior que 4 e menor que 40 caracteres."
+            }
+        }
     },
     slug: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
     }
 })
 
