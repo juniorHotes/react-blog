4fd0554ff625c25e2ac59bbc14330a55db7a5ead
juniorHotes <juniorhotes53@gmail.com>
2021-02-01 16:09:39 -0300

    Notify administrator a new comment.

--- server/src/database/controllers/client/CommentController.js
+++ server/src/database/controllers/client/CommentController.js
@@ -1,4 +1,7 @@
 const Comment = require('../../migrations/Comment')
+const Post = require('../../migrations/post')
+const SendEmail = require('../SendEmail')
+
 
 /*========== (POST) Insert comment  ==========*/
 const INSERT = async (req, res) => {
@@ -13,10 +16,28 @@ const INSERT = async (req, res) => {
         comment: comment,
         postId: postId
     }).then(() => {
-        res.sendStatus(200)
-    }).catch(err => {
-        res.sendStatus(404)
-    })
+        Post.findOne({where: { id: postId }})
+        .then(post => {
+            const styles = `
+                max-width: 120px;
+                display: block;
+                margin-top: 20px;
+                text-decoration: none;
+                padding: 12px;
+                background: #00adef;
+                color: white;
+                text-align: center;`
+
+            SendEmail.main({
+                subject: `${name} Fez um comentário - ${email}`,
+                html: `<p style="font-size:16px"><b>${name}</b> fez um comentário na postagem <b>${post.title}</b></p>
+                       <p>${comment}</p>
+                       <a style="${styles}" href='http://localhost:3333/post/${post.slug}'>Ver comentário</a>`
+            }).then(res.sendStatus(200)).catch(res.sendStatus(400))
+        
+            res.sendStatus(200)    
+        }).catch(err => { res.sendStatus(400)})
+    }).catch(err => {res.sendStatus(404) })
 }
 
 /*========== (POST) Update comment  ==========*/
