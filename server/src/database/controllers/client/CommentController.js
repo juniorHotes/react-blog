4a43154216d64960d029027a35258eeb958f1865
juniorHotes <juniorhotes53@gmail.com>
2021-02-03 14:37:33 -0300

    Email the administrator about a new comment.

--- server/src/database/controllers/client/CommentController.js
+++ server/src/database/controllers/client/CommentController.js
@@ -2,42 +2,21 @@ const Comment = require('../../migrations/Comment')
 const Post = require('../../migrations/post')
 const SendEmail = require('../SendEmail')
 
-
 /*========== (POST) Insert comment  ==========*/
 const INSERT = async (req, res) => {
-    const name = req.body.name
-    const email = req.body.email
-    const comment = req.body.comment
-    const postId = req.body.postId
+    const { name, email, comment, postId } = req.body
 
-    await Comment.create({
-        name: name,
-        email: email,
-        comment: comment,
-        postId: postId
-    }).then(() => {
-        Post.findOne({where: { id: postId }})
-        .then(post => {
-            const styles = `
-                max-width: 120px;
-                display: block;
-                margin-top: 20px;
-                text-decoration: none;
-                padding: 12px;
-                background: #00adef;
-                color: white;
-                text-align: center;`
+    try {
+        await Comment.create({ name, email, comment, postId })
+        const post = await Post.findOne({ where: { id: postId } })
 
-            SendEmail.main({
-                subject: `${name} Fez um comentário - ${email}`,
-                html: `<p style="font-size:16px"><b>${name}</b> fez um comentário na postagem <b>${post.title}</b></p>
-                       <p>${comment}</p>
-                       <a style="${styles}" href='http://localhost:3333/post/${post.slug}'>Ver comentário</a>`
-            }).then(res.sendStatus(200)).catch(res.sendStatus(400))
-        
-            res.sendStatus(200)    
-        }).catch(err => { res.sendStatus(400)})
-    }).catch(err => {res.sendStatus(404) })
+        SendEmail.main({
+            subject: `${name} Fez um comentário - ${email}`,
+            html: `<p style="font-size:16px"><b>${name}</b> fez um comentário na postagem <b>${post.title}</b></p>
+            <p>${comment}</p>
+            <a class="link" href='http://localhost:3333/post/${post.slug}'>Ver comentário</a>`
+        }).then(res.sendStatus(200)).catch(res.sendStatus(400))
+    } catch (err) { return res.status(404) }
 }
 
 /*========== (POST) Update comment  ==========*/
