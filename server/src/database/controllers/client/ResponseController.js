160f6c35fee7020165ecae26443a6897a3f6a02d
juniorHotes <juniorhotes53@gmail.com>
2021-02-05 17:15:43 -0300

    Refactoring the route to insert comment and update comment.

--- server/src/database/controllers/client/ResponseController.js
+++ server/src/database/controllers/client/ResponseController.js
@@ -12,38 +12,48 @@ const INSERT = async (req, res) => {
         const _comment = await Comment.findOne({ where: { id: commentId } })
         const post = await Post.findOne({ where: { id: _comment.postId } })
 
-        await SendEmail.main({
+        const sendUserEmail = {
             to: _comment.email,
             subject: `${name} Respondeu ao seu comentário - ${email}`,
             html: `<p style="font-size:16px"><b>${name}</b> 
                         Respondeu ao seu comentário na postagem 
                         <b>${post.title}</b></p>
                         <ul>
-                            <li><h3>${_comment.name}</h3></li>
+                            <li><p><b>${_comment.name}</b> Seu comentário</p></li>
                             <p>${_comment.comment}</p>
                             <ul>
                                 <li><p><b>${name}</b> Respondeu</p></li>
                                 <p>${comment}</p>
                             </ul>
                         </ul>
-                        <a class="link" href='http://localhost:3333/post/${post.slug}'>Responder</a>`
-        }).then(async () => {
-            await SendEmail.main({
-                subject: `${name} - Respondeu a um comentário - ${email}`,
-                html: `<p style="font-size:16px"><b>${name}</b> - 
-                            Respondeu ao comentário de <b>${_comment.name}</b> na postagem 
-                            <b>${post.title}</b></p>
+                        <a class="link" href='http://localhost:3333/post/${post.slug}'>Responder</a>
+                        <a class="link" href='http://localhost:3333/status-email/?status=unsubscribe&id=${_comment.id}'>
+                        Não receber mais Emails</a>`
+        }
+
+        const sendAdminEmail = {
+            subject: `${name} - Respondeu a um comentário - ${email}`,
+            html: `<p style="font-size:16px"><b>${name}</b> - 
+                        Respondeu ao comentário de <b>${_comment.name}</b> na postagem 
+                        <b>${post.title}</b></p>
+                        <ul>
+                            <li><p><b>${_comment.name}</b> Comentou</p></li>
+                            <p>${_comment.comment}</p>
                             <ul>
-                                <li><h3>${_comment.name}</h3></li>
-                                <p>${_comment.comment}</p>
-                                <ul>
-                                    <li><p><b>${name}</b> Respondeu</p></li>
-                                    <p>${comment}</p>
-                                </ul>
+                                <li><p><b>${name}</b> Respondeu</p></li>
+                                <p>${comment}</p>
                             </ul>
-                            <a class="link" href='http://localhost:3333/post/${post.slug}'>Ver todos os comentários</a>`
-            }).then(res.sendStatus(200))
-        })
+                        </ul>
+                        <a class="link" href='http://localhost:3333/post/${post.slug}'>Ver todos os comentários</a>`
+        }
+
+        if (_comment.status_email == false) {
+            await SendEmail.main(sendAdminEmail).then(res.sendStatus(200))
+        } else {
+            await SendEmail.main(sendAdminEmail).then(async () => {
+                await SendEmail.main(sendUserEmail).then(res.sendStatus(200))
+            })
+        }
     } catch (err) { return res.status(404) }
 }
 
@@ -51,9 +61,8 @@ const INSERT = async (req, res) => {
 const UPDATE = async (req, res) => {
     const { id, name, email, comment } = req.body
 
-    await Response.update({ name, email, comment }, {
-        where: { id: id }
-    }).then(() => res.sendStatus(200)).catch(err => res.sendStatus(404))
+    await Response.update({ name, email, comment }, { where: { id: id } })
+        .then(res.sendStatus(200)).catch(res.sendStatus(404))
 }
 
 /*========== (POST) Delete comment  ==========*/
