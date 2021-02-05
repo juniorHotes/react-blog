ba8e4f1bd5d6c98acc4456ee2be54df9a08e6e54
juniorHotes <juniorhotes53@gmail.com>
2021-02-05 17:13:54 -0300

    Route to receive email notification.

--- server/src/database/controllers/client/CommentController.js
+++ server/src/database/controllers/client/CommentController.js
@@ -2,12 +2,22 @@ const Comment = require('../../migrations/Comment')
 const Post = require('../../migrations/post')
 const SendEmail = require('../SendEmail')
 
+/*========== (GET) Status Email  ==========*/
+const statusEmail = async (req, res) => {
+    const status = req.query["status"]
+    const id = req.query["id"]
+
+    const status_email = status == "subscribe" ? true : false
+
+    await Comment.update({ status_email }, { where: { id: id } })
+    .then(res.sendStatus(200)).catch(res.sendStatus(400))
+}
 /*========== (POST) Insert comment  ==========*/
 const INSERT = async (req, res) => {
-    const { name, email, comment, postId } = req.body
+    const { name, email, comment, postId, status_email } = req.body
 
     try {
-        await Comment.create({ name, email, comment, postId })
+        await Comment.create({ name, email, comment, status_email, postId })
         const post = await Post.findOne({ where: { id: postId } })
 
         SendEmail.main({
@@ -18,7 +28,6 @@ const INSERT = async (req, res) => {
         }).then(res.sendStatus(200)).catch(res.sendStatus(400))
     } catch (err) { return res.status(404) }
 }
-
 /*========== (POST) Update comment  ==========*/
 const UPDATE = async (req, res) => {
     const { id, name, email, comment } = req.body
@@ -27,7 +36,6 @@ const UPDATE = async (req, res) => {
         where: { id: id }
     }).then(() => res.sendStatus(200)).catch(() => res.sendStatus(404))
 }
-
 /*========== (POST) Delete comment  ==========*/
 const DELETE = async (req, res) => {
     const id = req.body.id
@@ -36,6 +44,7 @@ const DELETE = async (req, res) => {
 }
 
 module.exports = {
+    statusEmail,
     INSERT,
     UPDATE,
     DELETE
