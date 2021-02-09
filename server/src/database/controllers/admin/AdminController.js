3740f8c1ebb283e3a6eac2c348ad4ad4acde84f9
juniorHotes <juniorhotes53@gmail.com>
2021-02-08 23:14:15 -0300

    Refactorings.

--- server/src/database/controllers/admin/AdminController.js
+++ server/src/database/controllers/admin/AdminController.js
@@ -16,94 +16,72 @@ const login = async (req, res) => {
                         id: user.id,
                         user: user.email
                     }
-                    res.send('Sessão criada')
-                    res.sendStatus(200)
+                    return res.json({ "msg": "Sessão criada" }).sendStatus(200)
                 } else {
-                    res.send('Email ou senha inválido')
-                    res.sendStatus(202)
+                    return res.json({ "msg": "Email ou senha inválido" }).sendStatus(202)
                 }
             } else {
-                res.send('Este usuário não está cadastrado!')
-                res.sendStatus(400)
+                return res.json({ "msg": "Este usuário não está cadastrado!" }).sendStatus(400)
             }
-        }).catch(() => res.sendStatus(404))
+        }).catch(err => res.json(err).sendStatus(404))
 }
 
 /*========== (POST) Create admin  ==========*/
 const INSERT = async (req, res) => {
     const { email, password } = req.body
 
-    await Admin.findOne({ where: { email: email } })
-        .then(user => {
-            if (user == undefined) {
-                const salt = bcryptjs.genSaltSync(10)
-                const hash = bcryptjs.hashSync(password, salt)
+    try {
+        const existsUser = await Admin.findOne({ where: { email: email } })
 
-                Admin.create({
-                    email: email,
-                    password: hash
-                }).then(() => {
-                    res.send("usuário criado")
-                    res.sendStatus(200)
-                }).catch(err => {
-                    res.send("Erro ao criar usuário: " + err)
-                    res.sendStatus(404)
-                })
-            } else {
-                res.send('Este usuário já existe!')
-                res.sendStatus(400)
-            }
-        })
+        if (existsUser == undefined) {
+            const salt = bcryptjs.genSaltSync(10)
+            const hash = bcryptjs.hashSync(password, salt)
+
+            await Admin.create({ email: email, password: hash })
+                .then(res.json({ "msg": "Usuário criado!" }).sendStatus(200))
+        } else {
+            return res.json({ "msg": "Este usuário já existe!" }).sendStatus(409)
+        }
+    } catch (err) { res.json(err).sendStatus(404) }
 }
 
 /*========== (POST) Update admin  ==========*/
 const UPDATE = async (req, res) => {
     const { email, password } = req.body
 
-    await Admin.findOne({ where: { email: email } })
-        .then(async user => {
-            if (user != undefined) {
-                const correct = bcryptjs.compareSync(password, user.password)
+    try {
+        const existsUser = await Admin.findOne({ where: { email: email } })
+        if (existsUser != undefined) {
+            const correct = bcryptjs.compareSync(password, existsUser.password)
 
-                if (correct) {
-                    const salt = bcryptjs.genSaltSync(10)
-                    const hash = bcryptjs.hashSync(password, salt)
+            if (correct) {
+                const salt = bcryptjs.genSaltSync(10)
+                const hash = bcryptjs.hashSync(password, salt)
 
-                    await Admin.update({
-                        password: hash
-                    }, {
-                        where: { email: email }
-                    }).then(() => {
-                        res.send('Senha alterada com sucesso!')
-                        res.sendStatus(200)
-                    }).catch(() => res.sendStatus(404))
-                } else {
-                    res.send('Email ou senha inválido')
-                    res.sendStatus(202)
-                }
+                await Admin.update({ password: hash }, { where: { email: email } })
+                    .then(res.json({ "msg": "Senha alterada com sucesso!" }).sendStatus(200))
             } else {
-                res.send('Usuário não encontrado!')
-                res.sendStatus(400)
+                return res.json({ "msg": "Email ou senha inválido!" }).sendStatus(202)
             }
-        }).catch(() => res.sendStatus(404))
+        } else {
+            return res.json({ "msg": "Usuário não encontrado!" }).sendStatus(400)
+        }
+    } catch (err) { res.json(err).sendStatus(404) }
+
 }
 
 /*========== (POST) Delete  ==========*/
 const DELETE = async (req, res) => {
     const email = req.body.email
 
-    await Admin.destroy({
-        where: { email: email }
-    }).then(() => {
-        res.send('User deleted')
-        res.sendStatus(200)
-    }).catch(() => res.sendStatus(404))
+    await Admin.destroy({ where: { email: email } })
+        .then(res.json({ "msg": "Administrador deletado!" }).sendStatus(200)).catch(err => res.json(err).sendStatus(404))
 }
 
 /*========== (GET) Logout admin  ==========*/
 const logout = async (req, res) => {
     req.session.user = undefined
-    res.sendStatus(200)
+    res.json({ "msg": "Sessão encerrada." }).sendStatus(200)
 }
 
 module.exports = {
