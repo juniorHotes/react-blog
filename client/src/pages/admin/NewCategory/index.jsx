c71795c1090c01e7bc21b7073b62739d83545ce0
juniorHotes <juniorhotes53@gmail.com>
2021-03-08 16:43:58 -0300

    Updated.

--- client/src/pages/admin/NewCategory/index.jsx
+++ client/src/pages/admin/NewCategory/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import HorizontalHeader from '../../../components/HorizontalHeader'
+import AdminOptions from '../../../components/AdminOptions'
+import Input from '../../../components/Input'
+import Button from '../../../components/Button'
 
 export default function NewCategory() {
     return (
-        <div>
+        <>
             <HorizontalHeader listOptions={
-                <Link to="/admin/profile">Editar perfil</Link>
+                <AdminOptions />
             } />
-        </div>
+            <div className='container'>
+                <h1>Nova Categoria</h1>
+                <hr/>
+                <form action={(e) => e.preventDefault}>
+                    <Input label="Título" type="text" name="new_category" placeholder='Nome da categoria'/>
+                    <Button primary>Salvar</Button>
+                </form>
+            </div>
+
+        </>
     )
 }
