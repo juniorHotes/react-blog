c08344099e4fe3668c17639c4f0fe26b7880ba43
juniorHotes <juniorhotes53@gmail.com>
2021-03-08 16:41:26 -0300

    Updated.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Home from './pages/Home'
+import Profile from './pages/admin/Profile'
 import NewCategory from './pages/admin/NewCategory'
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={NewCategory} />
-            {/* <Route path="/register" component={RegisterUser} />
-            <Route path="/login" component={Login} />
-            <Route path="/edit/user/:id" component={EditUser} /> */}
+            <Route path="/" exact component={Home} />
+            <Route path="/admin/profile" component={Profile} />
+            <Route path="/admin/new/category" component={NewCategory} />
         </BrowserRouter>
     )
 }
