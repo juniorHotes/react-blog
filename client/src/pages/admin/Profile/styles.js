e56c713f0aad5fdbeb0fc624d0a4145c430e1823
juniorHotes <juniorhotes53@gmail.com>
2021-03-15 23:50:25 -0300

    First commit.

---
+++ client/src/pages/admin/Profile/styles.js
@@ -0,0 +1,23 @@
+import Styled from 'styled-components'
+
+export const ProfileContainer = Styled.div`
+    display: flex;
+    flex-direction: row;
+`
+export const ImgContainer = Styled.div`
+    width: 16rem;
+`
+
+export const Img = Styled.img`
+    width: 100%;
+`
+
+export const Email = Styled.span`
+
+`
+
+export const Textarea = Styled.textarea`
+    width: 50%;
+    resize: vertical;
+
+`
