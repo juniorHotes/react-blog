d3ce009ba0e43a72f8d90fe63fa5c3ff4b7c8354
juniorHotes <juniorhotes53@gmail.com>
2021-03-16 18:31:26 -0300

    Removed rout NewCategory.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Profile from './pages/admin/Profile'
 import Category from './pages/admin/Category'
-import NewCategory from './pages/admin/NewCategory'
 import Post from './pages/admin/Post'
 import NewPost from './pages/admin/NewPost'
 
@@ -14,7 +13,6 @@ export default function Routes() {
             <Route path="/" exact component={Home} />
             <Route path="/admin/profile" component={Profile} />
             <Route path="/admin/categories/:page" component={Category} />
-            <Route path="/admin/new/category" component={NewCategory} />
             <Route path="/admin/posts/:page" component={Post} />
             <Route path="/admin/post/new" component={NewPost} />
         </BrowserRouter>
