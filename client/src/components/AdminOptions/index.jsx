6ac56a22030f3bfea17ac50649cb52fdb76537ef
juniorHotes <juniorhotes53@gmail.com>
2021-03-15 23:48:48 -0300

    New routes.

--- client/src/components/AdminOptions/index.jsx
+++ client/src/components/AdminOptions/index.jsx
@@ -7,9 +7,7 @@ export default function AdminOptions() {
         <ul>
             <li><Link to="/admin/profile">Editar Perfil</Link></li>
             <li><Link to="/admin/categories/1">Categorias</Link></li>
-            <li><Link to="/admin/new/category">Nova Categoria</Link></li>
             <li><Link to="/admin/posts/1">Postagens</Link></li>
-            <li><Link to="/admin/post/new">Nova Postagem</Link></li>
             <li><Link to="/">Home</Link></li>
         </ul>
     )
