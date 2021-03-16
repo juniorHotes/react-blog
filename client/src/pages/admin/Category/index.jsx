512952fd799c9d764f9ce4c541d25d5211c43198
juniorHotes <juniorhotes53@gmail.com>
2021-03-15 23:49:55 -0300

    Insert tables.

--- client/src/pages/admin/Category/index.jsx
+++ client/src/pages/admin/Category/index.jsx
@@ -1,22 +1,74 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import api from '../../../services/api'
 import HorizontalHeader from '../../../components/HorizontalHeader'
 import AdminOptions from '../../../components/AdminOptions'
+import Button from '../../../components/Button'
+import Input from '../../../components/Input'
+
+import IconEdit from '../../../assets/icons/edit.svg'
+import IconDelete from '../../../assets/icons/delete.svg'
 
 export default function Category(props) {
-    console.log(props.location.pathname)
+    const [categories, setCategories] = useState([])
 
-    useEffect( async () => {
+    useEffect(async () => {
         const { data } = await api.get(props.location.pathname)
-        console.log(data)
+        setCategories(data.categories.rows)
     }, [])
+
+    function dateFormat(date) {
+        const fullDate = new Date(`${date}`)
+
+        function convert(n) { return n < 10 ? `0${n}` : n }
+
+        const dd = convert(fullDate.getDate())
+        const mm = convert(fullDate.getMonth() + 1)
+        const yy = fullDate.getFullYear()
+        const hrs = convert(fullDate.getHours())
+        const mts = convert(fullDate.getMinutes())
+
+        return `${dd}/${mm}/${yy} às ${hrs}:${mts}`
+    }
 
     return (
         <>
             <HorizontalHeader listOptions={
                 <AdminOptions />
             } />
+
             <div className='container'>
+                <h1>Categorias</h1>
+                <hr />
+
+                <form action={(e) => e.preventDefault}>
+                    <Input label="Nova categoria" type="text" name="new_category" placeholder='Nome da categoria' />
+                    <Button primary>Salvar</Button>
+                </form>
+
+                <table>
+                    <tbody>
+                        <th>ID</th>
+                        <th>Título</th>
+                        <th>Criado</th>
+                        <th>Editado</th>
+                        <th></th>
+                    </tbody>
+
+                    {categories.map(cat => {
+                        return (
+                            <tbody key={cat.id}>
+                                <td>{cat.id}</td>
+                                <td>{cat.title}</td>
+                                <td>{dateFormat(cat.createdAt)}</td>
+                                <td>{dateFormat(cat.updatedAt)}</td>
+                                <td>
+                                    <Button><img src={IconEdit} /></Button>
+                                    <Button danger><img src={IconDelete} /></Button>
+                                </td>
+                            </tbody>
+                        )
+                    })}
+                </table>
             </div>
         </>
     )
--- client/src/pages/admin/Post/index.jsx
+++ client/src/pages/admin/Post/index.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import api from '../../../services/api'
 import HorizontalHeader from '../../../components/HorizontalHeader'
 import AdminOptions from '../../../components/AdminOptions'
+import Button from '../../../components/Button'
+
+import IconEdit from '../../../assets/icons/edit.svg'
+import IconDelete from '../../../assets/icons/delete.svg'
 
 export default function Post(props) {
     const [posts, setPosts] = useState([])
@@ -11,34 +15,55 @@ export default function Post(props) {
         setPosts(data.post.rows)
     }, [])
 
+    function dateFormat(date) {
+        const fullDate = new Date(`${date}`)
+
+        function convert(n) { return n < 10 ? `0${n}` : n }
+
+        const dd = convert(fullDate.getDate())
+        const mm = convert(fullDate.getMonth() + 1)
+        const yy = fullDate.getFullYear()
+        const hrs = convert(fullDate.getHours())
+        const mts = convert(fullDate.getMinutes())
+
+        return `${dd}/${mm}/${yy} às ${hrs}:${mts}`
+    }
+
     return (
         <>
             <HorizontalHeader listOptions={
                 <AdminOptions />
             } />
-            {console.log(posts)}
+            
             <div className='container'>
                 <h1>Postagens</h1>
-                <hr/>
+                <hr />
+
+                <Button primary>Nova Postagem</Button>
 
                 <table>
-                    <tr>
+                    <tbody>
                         <th>ID</th>
                         <th>Título</th>
-                        <th>Criado</th>
+                        <th>Postado</th>
                         <th>Editado</th>
                         <th>Categoria</th>
-                    </tr>
+                        <th></th>
+                    </tbody>
 
                     {posts.map(post => {
                         return (
-                            <tr>
+                            <tbody key={post.id}>
                                 <td>{post.id}</td>
                                 <td>{post.title}</td>
-                                <td>{post.createdAt}</td>
-                                <td>{post.updatedAt}</td>
+                                <td>{dateFormat(post.createdAt)}</td>
+                                <td>{dateFormat(post.updatedAt)}</td>
                                 <td>{post.category.title}</td>
-                            </tr>
+                                <td>
+                                    <Button><img src={IconEdit} /></Button>
+                                    <Button danger><img src={IconDelete} /></Button>
+                                </td>
+                            </tbody>
                         )
                     })}
                 </table>
