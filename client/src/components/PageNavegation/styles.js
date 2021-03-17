45e29c65e2596b62e1ea256bdb3c6a6997932d8b
juniorHotes <juniorhotes53@gmail.com>
2021-03-17 18:08:00 -0300

    First commit.

---
+++ client/src/components/PageNavegation/styles.js
@@ -0,0 +1,9 @@
+import Styled from 'styled-components'
+
+export const Container = Styled.div`
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+`
