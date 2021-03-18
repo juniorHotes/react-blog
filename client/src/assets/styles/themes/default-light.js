521f226cf51b2e6ee23770f4db2de084dc9d3dc8
juniorHotes <juniorhotes53@gmail.com>
2021-03-18 10:34:18 -0300

    First commit.

---
+++ client/src/assets/styles/themes/default-light.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'default-light',
+
+    color: {
+        // GLOBAL COLOR
+        global_primary_color: '#212F3C',
+        global_secundary_color: '#17202A',
+        // GLOBAL TEXT COLOR
+        global_title_color: '#17202A',
+        global_text_color: '#212F3C',
+        // HEADER COLOR
+        bg_header_color: '#212F3C',
+        bg_header_button_color: '#17202A',
+        title_header_color: 'white',
+        text_header_color: 'white',
+        // SECTION COLOR
+        bg_section_color: 'white'
+    }
+}
