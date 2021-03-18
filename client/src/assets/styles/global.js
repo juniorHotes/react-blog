64b0e7b96c89dfccda5f897f4664aa489c301b3f
juniorHotes <juniorhotes53@gmail.com>
2021-03-18 10:34:04 -0300

    Refactoring.

--- client/src/assets/styles/global.js
+++ client/src/assets/styles/global.js
@@ -1,53 +1,58 @@
 import { createGlobalStyle } from 'styled-components'
 
-export const defaultTheme = {
-    bg_header_color: '#212F3C'
-}
-
-export const GlobalStyle = createGlobalStyle`
-    & * {
+export default createGlobalStyle`
+    * {
         margin: 0;
         box-sizing: border-box;
     }
 
-    & html {
+    html {
         scroll-behavior: smooth;
     }
 
-    & #root {
+    body {
+        background-color: ${props => props.theme.color.bg_section_color};
+    }
+
+    #root {
         max-width: 1600px;
         margin: 0 auto;
         display: grid;
         grid-template-columns: 20rem 2fr;
     }
 
-    & .container {
+    .container {
         margin: 16px;
         padding: 8px;
     }
 
-    & table {
+    h1, h2, h3, h4, h5, h6 {
+        color: ${props => props.theme.color.global_title_color}
+    }
+
+    table {
         width: 100%;
     }
 
-    & table thead tr th {
+    table thead tr th {
        padding: .5rem;
-       background-color: ${defaultTheme.bg_header_color};
+       background-color: white;
        text-align: start;
-       color: white;
+       color: ${props => props.theme.color.global_title_color}
     }
 
-    & table thead tr th:first-child {
+    table thead tr th:first-child {
         width: 1rem;
         min-width: 1rem;
      }
 
-    & table thead tr th:last-child {
+    table thead tr th:last-child {
         width: 11rem;
      }
  
-    & table td {
+    table td {
        padding: .5rem;
        border: 1px solid #c9c9c9;
+       color: ${props => props.theme.color.global_text_color}
     }
 `
