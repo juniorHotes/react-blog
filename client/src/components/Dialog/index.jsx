1ceccafe26a53b864e321a109228be367f759252
juniorHotes <juniorhotes53@gmail.com>
2021-03-18 18:11:09 -0300

    First commit.

---
+++ client/src/components/Dialog/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Modal, Header } from './styles'
+
+export default function Dialog(props) {
+    return (
+        <Container>
+            <Modal>
+                <Header>Header</Header>
+            </Modal>
+        </Container>
+    )
+}
---
+++ client/src/components/Dialog/styles.js
@@ -0,0 +1,25 @@
+import Styled from 'styled-components'
+
+export const Container = Styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+`
+export const Modal = Styled.div`
+    min-width: 450px;
+    min-height: 400px;
+    margin-top: 5rem;
+    padding: 1.5rem;
+    border-radius: 4px;
+    background-color: white;
+`
+export const Header = Styled.header`
+    
+`
