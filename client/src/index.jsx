d526099acdaea459553cd3bb6e9ed6733bba62a0
juniorHotes <juniorhotes53@gmail.com>
2021-03-18 10:33:30 -0300

    Theme.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { GlobalStyle } from './assets/styles/global.js'
+import GlobalStyle from './assets/styles/global.js'
+import { ThemeProvider } from 'styled-components'
+import defaultLight from './assets/styles/themes/default-light'
+import defaultDark from './assets/styles/themes/default-dark'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={defaultLight} >
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
