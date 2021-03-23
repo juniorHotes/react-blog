1e275d5f7558872322d0948840fabd2467c15146
juniorHotes <juniorhotes53@gmail.com>
2021-03-22 23:40:13 -0300

    Dialog component.

--- client/src/components/Dialog/index.jsx
+++ client/src/components/Dialog/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { Container, Modal, Header } from './styles'
+import React, { useState } from 'react'
+import { Container, Modal, Header, Body} from './styles'
+
+import IconClose from '../../assets/icons/bx-x.svg'
+import Button from '../Button'
 
 export default function Dialog(props) {
+    const [close, setClose] = useState(props.hidden)
+
     return (
-        <Container>
+        <Container hidden={close}>
             <Modal>
-                <Header>Header</Header>
+                <Header>
+                    {props.title} 
+                    <Button onClick={() => setClose(true)}>
+                        <img src={IconClose} alt="Close"/>
+                    </Button>
+                </Header>
+                <Body>
+                    {props.body}
+                </Body>
             </Modal>
         </Container>
     )
