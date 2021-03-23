cfa022b9b4214a07ff1b7860b8d8915a41782498
juniorHotes <juniorhotes53@gmail.com>
2021-03-22 23:40:37 -0300

    Dialog styles.

--- client/src/components/Dialog/styles.js
+++ client/src/components/Dialog/styles.js
@@ -8,9 +8,11 @@ export const Container = Styled.div`
     top: 0;
     left: 0;
     z-index: 50;
-    display: flex;
+    display: ${props => props.hidden ? 'none' : 'flex'};
     justify-content: center;
     align-items: flex-start;
+    transition: width .5s;
+    
 `
 export const Modal = Styled.div`
     min-width: 450px;
@@ -21,5 +23,22 @@ export const Modal = Styled.div`
     background-color: white;
 `
 export const Header = Styled.header`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.8rem;
+    font-weight: bold;
+
+    & button {
+        margin: 0 0 0 12px;
+        padding: 4px;
+    }
+
+    & img {
+        width: 32px;
+    }
+`
+
+export const Body = Styled.div`
+    width: 100%
 `
